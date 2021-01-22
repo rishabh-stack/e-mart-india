da00e9ae7295f4e4888149a5a36d9a366c5d34de
Rishabh Sharma <rishabhsharma952000@gmail.com>
2021-01-23 00:09:07 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -123,9 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 #managing media
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
